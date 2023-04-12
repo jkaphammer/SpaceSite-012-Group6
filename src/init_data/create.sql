ad81ae6d1de5fa0ffb05338e4da4deac786fa169
Elijah White <elwh8851@colorado.edu>
2023-04-12 15:57:31 -0600

    added datatable initialization

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -1,1 +1,8 @@
 -- test
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users(
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) PRIMARY KEY,
+    password CHAR(60) NOT NULL,
+    birthday DATE
+);
