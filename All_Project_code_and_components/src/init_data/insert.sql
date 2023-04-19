850ba32427bab3655775c66342e3a783b953ece4
Brian Ramirez <brra7685@colorado.edu>
2023-04-18 23:50:12 -0600

    Added test cases, uptated directory, insert.sql, index.js

---
+++ All_Project_code_and_components/src/init_data/insert.sql
@@ -0,0 +1,4 @@
+-- test
+INSERT INTO users (name, email, password, birthday) VALUES 
+('John Doe', 'johndoe@example.com', 'HelloWorld', '2000-01-01'),
+('John Deer', 'johndeer@example.com', 'HelloWorld0', '2000-01-01');
