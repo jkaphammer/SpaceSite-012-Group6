600e71aed1adedc32f07b1b933326c6e4f6cfd1a
Elijah White <elwh8851@colorado.edu>
2023-04-26 14:35:57 -0600

    added databases

--- All_Project_code_and_components/src/init_data/create.sql
+++ All_Project_code_and_components/src/init_data/create.sql
@@ -6,3 +6,16 @@ CREATE TABLE users(
     password CHAR(60) NOT NULL,
     birthday DATE
 );
+
+DROP TABLE IF EXISTS comments CASCADE;
+CREATE TABLE comments(
+    comment VARCHAR(500) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    comment_date DATE NOT NULL
+);
+
+DROP TABLE IF EXISTS user_likes CASCADE;
+CREATE TABLE user_likes(
+    email VARCHAR(100) NOT NULL REFERENCES users(email),
+    picture_url VARCHAR(200) NOT NULL
+);
